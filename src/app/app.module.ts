a27968bcf58ceeb6ee5228c9f704caeb722b5cec
taitamfc <taitam8590@gmail.com>
2021-12-20 11:02:01 +0700

    interface product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDeleteComponent } from './product-delete/product-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductEditComponent,
+    ProductAddComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule
